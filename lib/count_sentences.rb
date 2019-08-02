69e03a2f2aceefb145214836ceaf947fa27da6b7
Ronald Stephens <rstephens@bluemorpho.cc>
2019-08-02 00:46:55 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -16,7 +16,7 @@ class String
 
   def count_sentences
     count = 0
-    if self.split(".", "!", "?")
+    if self.end_with?(".") || self.end_with?("?") || self.end_with?("!")
       count += 1
     end
     count
